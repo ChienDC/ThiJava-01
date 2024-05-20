52f258f63b6fec7a26baefcd0e58541fb010da6a
ChienDC <doancaochien.1994@gmail.com>
2024-05-20 19:06:51 +0700

    update bankmain

--- src/BankMain.java
+++ src/BankMain.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class BankMain  {
+public class BankMain {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -11,28 +11,21 @@ public class BankMain  {
         // Nhập thông tin từ bàn phím
         System.out.print("Enter account number for account 1: ");
         account1.setAccountNumber(scanner.nextLine());
-
         System.out.print("Enter account holder name for account 1: ");
         account1.setAccountHolderName(scanner.nextLine());
-
         System.out.print("Enter initial balance for account 1: ");
         account1.setBalance(scanner.nextDouble());
-
-        scanner.nextLine(); // Consume newline left-over
-
+        scanner.nextLine();
         System.out.print("Enter account number for account 2: ");
         account2.setAccountNumber(scanner.nextLine());
-
         System.out.print("Enter account holder name for account 2: ");
         account2.setAccountHolderName(scanner.nextLine());
-
         System.out.print("Enter initial balance for account 2: ");
         account2.setBalance(scanner.nextDouble());
 
         // Hiển thị thông tin tài khoản
         System.out.println("\nAccount 1 Info:");
         account1.displayAccountInfo();
-
         System.out.println("\nAccount 2 Info:");
         account2.displayAccountInfo();
 
@@ -45,10 +38,8 @@ public class BankMain  {
         // Hiển thị thông tin tài khoản sau khi thực hiện các giao dịch
         System.out.println("\nAccount 1 Info After Transactions:");
         account1.displayAccountInfo();
-
         System.out.println("\nAccount 2 Info After Transactions:");
         account2.displayAccountInfo();
-
         scanner.close();
     }
 }
